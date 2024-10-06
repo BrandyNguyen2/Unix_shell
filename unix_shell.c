938790e25a2c05072637584560b32019d2bb2152
bnguyen700 <brandy.nguyen02@gmail.com>
2024-10-05 22:49:16 -0700

    final unix_shell

--- unix_shell.c
+++ unix_shell.c
@@ -9,7 +9,7 @@
 
 #define MAX_INPUT_SIZE 1024
 
-// 1, 2, & 3 (GOOD): parses commands and echo them back
+// 1, 2, & 3: parses commands and echo them back
 void parse_echo(char *input) 
 {
     for (int i = 0; input[i] != '\0'; i++) 
@@ -31,19 +31,23 @@ void parse_echo(char *input)
             }
             char word[MAX_INPUT_SIZE];
             strncpy(word, &input[start], i - start);
-            word[i - start] = '\0'; 
+            word[i - start] = '\0';
             printf("%s\n", word);
             i--; 
         }
     }
 }
 
-// 4 (!! still needs to be fixed): run built-in commands
+// 4: run built-in commands
 void built_in_commands(char *input, char *last_command) 
 {
-    if (strcmp(input, "help") == 0) 
-    {
-        printf("Available commands: help, cd, mkdir, exit, !!\n");
+    if (strcmp(input, "help") == 0) {
+        printf("Available commands:\n");
+        printf("help: displays available commands\n");
+        printf("cd: changes directory\n");
+        printf("mkdir: creates a new directory\n");
+        printf("exit: exits the shell\n");
+        printf("!!: reruns the last command\n");
     } 
     else if (strncmp(input, "cd", 2) == 0) 
     {
@@ -65,151 +69,194 @@ void built_in_commands(char *input, char *last_command)
     {
         exit(0);
     } 
-    else if (strcmp(input, "!!") == 0 && last_command != NULL) 
+    else if (strcmp(input, "!!") == 0) 
     {
-        printf("Running last command: %s\n", last_command);
-        strcpy(input, last_command);  // Re-run the last command
+        if (last_command[0] != '\0') 
+        {
+            strcpy(input, last_command); 
+        } 
+        else 
+        {
+            printf("No previous command.\n");
+            input[0] = '\0'; // 
+        }
     }
 }
-// 5 (GOOD): execute without command line redirection
-void execute_command(char *input) {
+
+// 5 : execute without command line redirection (just like fork() from hw)
+void execute_command(char *input) 
+{
     pid_t pid = fork();
 
-    if (pid == 0) {
+    if (pid == 0) 
+    {
         // Child process
         char *args[] = {input, NULL};
         execvp(args[0], args);
         perror("exec failed");
         exit(1);
-    } else if (pid > 0) {
+    } 
+    else if (pid > 0) 
+    {
         // Parent process waits for child to finish
         wait(NULL);
-    } else {
+    } 
+    else 
+    {
         perror("fork failed");
     }
 }
 
-// 6 (GOOD?): execute with command line redirection
-void execute_with_redirection(char *input) {
+// 6: execute with command line redirection
+void execute_with_redirection(char *input) 
+{
     char *cmd = strtok(input, ">");
     char *filename = strtok(NULL, " ");
     
     pid_t pid = fork();
     
-    if (pid == 0) {
-        // Child process
+    if (pid == 0)
+    {
         int fd = open(filename, O_WRONLY | O_CREAT, 0644);
-        dup2(fd, STDOUT_FILENO); // Redirect stdout to file
+        dup2(fd, STDOUT_FILENO); 
         close(fd);
         
         char *args[] = {cmd, NULL};
         execvp(args[0], args);
         //perror("exec failed!");
         exit(1);
-    } else if (pid > 0) {
-        // Parent process waits for child to finish
+    } 
+    else if (pid > 0) 
+    {
         wait(NULL);
-    } else {
+    } 
+    else 
+    {
         perror("fork failed");
     }
 }
 
-// 7 (GOOD): execute two commands connected by a pipe
-void process_pipe_cmds(char **argv1, char **argv2) {
-  int p[2];
+// 7 (John Carlo Manuel helped me with this function): execute two commands connected by a pipe
+void process_pipe_cmds(char **argv1, char **argv2) 
+{
+    int p[2];
 
-  if (pipe(p) < 0) {
-    perror("Pipe failed");
-    exit(1);
-  }
-
-  pid_t pid1 = fork();
-
-  if (pid1 < 0) {
-    perror("Fork for first command failed");
-    exit(1);
-  }
-
-  if (pid1 == 0) {
-    dup2(p[1], STDOUT_FILENO);
-    close(p[0]);
-    close(p[1]);
-
-    if (execvp(argv1[0], argv1) < 0) {
-      printf("Execution of first command failed");
-      exit(1);
-    }
-  }
-
-  pid_t pid2 = fork();
-
-  if (pid2 < 0) 
-  {
-    printf("Fork for second command failed");
-    exit(1);
-  }
-
-  if (pid2 == 0) 
-  {
-    dup2(p[0], STDIN_FILENO);
-    close(p[1]);
-    close(p[0]);
-
-    if (execvp(argv2[0], argv2) < 0) 
+    if (pipe(p) < 0) 
     {
-      printf("Execution of second command failed");
-      exit(1);
+        perror("Pipe failed");
+        exit(1);
     }
-  }
 
-  close(p[0]);
-  close(p[1]);
+    pid_t pid1 = fork();
 
-  waitpid(pid1, NULL, 0);
-  waitpid(pid2, NULL, 0);
+    if (pid1 < 0) 
+    {
+        perror("Fork for first command failed");
+        exit(1);
+    }
+
+    if (pid1 == 0) 
+    {
+        dup2(p[1], STDOUT_FILENO);
+        close(p[0]);
+        close(p[1]);
+
+        if (execvp(argv1[0], argv1) < 0) 
+        {
+        printf("Execution of first command failed");
+        exit(1);
+        }
+    }
+
+    pid_t pid2 = fork();
+
+    if (pid2 < 0) 
+    {
+        printf("Fork for second command failed");
+        exit(1);
+    }
+
+    if (pid2 == 0) 
+    {
+        dup2(p[0], STDIN_FILENO);
+        close(p[1]);
+        close(p[0]);
+
+        if (execvp(argv2[0], argv2) < 0) 
+        {
+        printf("Execution of second command failed");
+        exit(1);
+        }
+    }
+
+    close(p[0]);
+    close(p[1]);
+
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
 }
 
-void parse_command(char *command, char **argv)
+void parse_command(char *command, char **argv) 
 {
     int index = 0;
     char *token = strtok(command, " ");
-    while (token != NULL) {
+    while (token != NULL) 
+    {
         argv[index++] = token;
         token = strtok(NULL, " ");
     }
     argv[index] = NULL; 
 }
 
-int main()
+int main() 
 {
     char input[MAX_INPUT_SIZE];
     char last_command[MAX_INPUT_SIZE] = {0};
 
-    while (1) {
+    while (1) 
+    {
         printf("shell> ");
         fgets(input, MAX_INPUT_SIZE, stdin);
-        input[strcspn(input, "\n")] = 0; 
-        // Save the input as last command for !!
-        strcpy(last_command, input);
-        // Check for special commands
-        if (strstr(input, "ECHO") != NULL)
+        input[strcspn(input, "\n")] = 0;
+
+        if (strcmp(input, "!!") == 0) 
+        {
+            if (last_command[0] != '\0') 
+            {
+                strcpy(input, last_command); 
+            } 
+            else 
+            {
+                printf("No previous command.\n");
+                continue;
+            }
+        } 
+        else 
+        {
+            strcpy(last_command, input); 
+        }
+        if (strstr(input, "ECHO") != NULL) 
         {
             parse_echo(input);
         } 
-        else if (strstr(input, "help") || strstr(input, "cd")  
-        || strstr(input, "mkdir") || strstr(input, "exit") || strstr(input, "!!"))
+        else if (strstr(input, "help") || strstr(input, "cd") || strstr(input, "mkdir") 
+        || strstr(input, "exit") || strcmp(input, "!!") == 0) 
         {
             built_in_commands(input, last_command);
+            if (input[0] == '\0') 
+            {
+                continue; 
+            }
         } 
         else if (strchr(input, '>') != NULL) 
         {
             execute_with_redirection(input);
         } 
-        else if (strchr(input, '|') != NULL)
+        else if (strchr(input, '|') != NULL) 
         {
             char *cmd1 = strtok(input, "|");
             char *cmd2 = strtok(NULL, "|");
-            if (cmd1 != NULL && cmd2 != NULL)
+            if (cmd1 != NULL && cmd2 != NULL) 
             {
                 char *argv1[MAX_INPUT_SIZE];
                 char *argv2[MAX_INPUT_SIZE];
@@ -217,12 +264,12 @@ int main()
                 parse_command(cmd2, argv2);
                 process_pipe_cmds(argv1, argv2);
             }
-        }
-        else 
+        } 
+        else if (input[0] != '\0') 
         {
             execute_command(input);
         }
     }
-
+    
     return 0;
 }
